027eda5d9ed270c0f9daf11ae3d4b108e1b4536c
Rodrigo Dalmas <rodrigo.dalmas@hp.com>
2017-11-07 14:03:14 -0200

    Adding index file to transitions folder

---
+++ src/transitions/index.js
@@ -0,0 +1,6 @@
+// @flow
+
+export { default as Collapse } from './Collapse';
+export { default as Fade } from './Fade';
+export { default as Grow } from './Grow';
+export { default as Slide } from './Slide';
