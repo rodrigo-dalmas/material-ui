336c10b9745c2f7be7395633b97e1a3b53c40bcf
Rodrigo Dalmas <rodrigo.dalmas@hp.com>
2017-11-07 14:11:51 -0200

    Adding Slide to the index file

--- src/transitions/index.js
+++ src/transitions/index.js
@@ -1,6 +1,6 @@
 // @flow
 
+export { default } from './Slide';
 export { default as Collapse } from './Collapse';
 export { default as Fade } from './Fade';
 export { default as Grow } from './Grow';
-export { default as Slide } from './Slide';
