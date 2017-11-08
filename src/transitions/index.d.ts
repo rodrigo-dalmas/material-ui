2108a3dd52a081b090ee4bc5c7ea5682b6ce170b
Rodrigo Dalmas <rodrigo.dalmas@hp.com>
2017-11-08 10:30:13 -0200

    Adding index.d.ts

--- src/transitions/index.d.ts
+++ src/transitions/index.d.ts
@@ -1,6 +1,8 @@
-// @flow
-
 export { default as Slide } from './Slide';
+export * from './Slide';
 export { default as Collapse } from './Collapse';
+export * from './Collapse';
 export { default as Fade } from './Fade';
+export * from './Fade';
 export { default as Grow } from './Grow';
+export * from './Grow';
